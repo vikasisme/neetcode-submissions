42ab92cbdf1d4ce9608138db0531129c5d10ce0a
Vikas <15150410+vikasisme@users.noreply.github.com>
2026-09-16 22:20:22 -0700

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const map = new Map();
+
+        for(let i=0; i<nums.length; i++){
+
+            let diff = target - nums[i];
+
+            if(map.has(diff)){
+                return [map.get(diff), i];
+            }
+
+            map.set(nums[i], i);
+        }
+
+        return [];
+
+    }
+}
